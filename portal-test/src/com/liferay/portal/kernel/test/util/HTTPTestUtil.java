8df8e07a8669a76abadf9e4b11552b65a14cc7fc
Jaime Leon <jaime.leon@liferay.com>
2024-04-08 13:00:57 +0300

    LPD-21413 Use provided utils

--- portal-test/src/com/liferay/portal/kernel/test/util/HTTPTestUtil.java
+++ portal-test/src/com/liferay/portal/kernel/test/util/HTTPTestUtil.java
@@ -14,6 +14,7 @@ import com.liferay.portal.kernel.util.Base64;
 import com.liferay.portal.kernel.util.ContentTypes;
 import com.liferay.portal.kernel.util.Http;
 import com.liferay.portal.kernel.util.HttpUtil;
+import com.liferay.portal.kernel.util.MapUtil;
 
 import java.nio.charset.StandardCharsets;
 
@@ -130,7 +131,7 @@ public class HTTPTestUtil {
 		options.addHeader(
 			"Authorization", "Basic " + Base64.encode(_credentials.getBytes()));
 
-		if (headers != null) {
+		if (MapUtil.isNotEmpty(headers)) {
 			for (HashMap.Entry<String, String> header : headers.entrySet()) {
 				options.addHeader(header.getKey(), header.getValue());
 			}
