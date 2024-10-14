7abe043866d8872ada82b275e87be9f34d1c176e
Carlos Correa <carlos.correa@liferay.com>
2024-10-14 11:47:30 +0200

    LPD-39238 Inline

--- modules/apps/headless/headless-builder/headless-builder-web/src/main/resources/META-INF/resources/js/components/baseComponents/BaseAPISchemaProperty.tsx
+++ modules/apps/headless/headless-builder/headless-builder-web/src/main/resources/META-INF/resources/js/components/baseComponents/BaseAPISchemaProperty.tsx
@@ -74,15 +74,14 @@ export default function BaseAPISchemaProperty({
 		});
 	};
 
-	const localizedPropertyName =
-		objectField.label[Liferay.ThemeDisplay.getDefaultLanguageId()]!;
-
 	return (
 		ALLOWED_BUSINESS_TYPES.includes(objectField.businessType) && (
 			<ClayButton
 				aria-label={sub(
 					Liferay.Language.get('add-x-property'),
-					localizedPropertyName
+					objectField.label[
+						Liferay.ThemeDisplay.getDefaultLanguageId()
+					]!
 				)}
 				className="property-container"
 				displayType="unstyled"
