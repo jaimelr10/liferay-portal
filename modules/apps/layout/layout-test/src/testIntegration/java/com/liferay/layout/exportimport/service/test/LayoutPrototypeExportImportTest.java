ba1c7f494bff0526c1ba4b3fd5a2f3d6d5b19caf
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2024-10-15 11:53:45 +0200

    LPD-39356 Migrates ImportLAR test to integration

---
+++ modules/apps/layout/layout-test/src/testIntegration/java/com/liferay/layout/exportimport/service/test/LayoutPrototypeExportImportTest.java
@@ -0,0 +1,87 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.layout.exportimport.service.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.exportimport.test.util.lar.BaseExportImportTestCase;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.model.LayoutPrototype;
+import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.service.LayoutPrototypeLocalService;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@RunWith(Arquillian.class)
+public class LayoutPrototypeExportImportTest extends BaseExportImportTestCase {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Before
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+
+		UserTestUtil.setUser(TestPropsValues.getUser());
+
+		_company = _companyLocalService.getCompany(
+			TestPropsValues.getCompanyId());
+	}
+
+	@Test
+	public void testExportImportLayoutPrototype() throws Exception {
+		group = _company.getGroup();
+
+		importedGroup = _company.getGroup();
+
+		LayoutPrototype layoutPrototype =
+			_layoutPrototypeLocalService.addLayoutPrototype(
+				TestPropsValues.getUserId(), _company.getCompanyId(),
+				HashMapBuilder.put(
+					LocaleUtil.getDefault(), RandomTestUtil.randomString()
+				).build(),
+				null, true, ServiceContextTestUtil.getServiceContext());
+
+		exportLayouts(new long[0], getExportParameterMap(), false);
+
+		_layoutPrototypeLocalService.deleteLayoutPrototype(layoutPrototype);
+
+		importLayouts(getImportParameterMap(), false);
+
+		layoutPrototype = _layoutPrototypeLocalService.getLayoutPrototype(
+			_company.getCompanyId(),
+			layoutPrototype.getName(LocaleUtil.getDefault()));
+
+		_layoutPrototypeLocalService.deleteLayoutPrototype(layoutPrototype);
+	}
+
+	private Company _company;
+
+	@Inject
+	private CompanyLocalService _companyLocalService;
+
+	@Inject
+	private LayoutPrototypeLocalService _layoutPrototypeLocalService;
+
+}
