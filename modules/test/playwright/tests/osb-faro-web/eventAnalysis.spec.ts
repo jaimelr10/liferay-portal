e6cf3b36c740636db54686bacc0acd12ec62b685
rafaellajordao <rafaella.jordao@liferay.com>
2024-10-24 09:43:51 -0300

    LPD-34328 add 'Change data type with event already in use' test

--- modules/test/playwright/tests/osb-faro-web/eventAnalysis.spec.ts
+++ modules/test/playwright/tests/osb-faro-web/eventAnalysis.spec.ts
@@ -24,10 +24,11 @@ import {
 	removeAttribute,
 	setEventAnalysisName,
 } from './utils/events';
-import {ACPage, navigateToACPageViaURL} from './utils/navigation';
+import {ACPage, navigateToACSettingsViaURL, navigateToACPageViaURL} from './utils/navigation';
 import {createSitePage, navigateToSitePage} from './utils/portal';
 import {closeSessions} from './utils/sessions';
 import {changeTimeFilter} from './utils/time-filter';
+import { selectAndExpectToHaveValue } from '../../utils/selectAndExpectToHaveValue';
 
 export const test = mergeTests(
 	apiHelpersTest,
@@ -88,6 +89,223 @@ test.afterEach(async ({apiHelpers, page}) => {
 		await apiHelpers.headlessSite.deleteSite(String(site.id));
 	});
 });
+
+test(
+	'Change data type with event already in use',
+	{
+		tag: '@LRAC-6280',
+	},
+
+	async ({
+		apiHelpers,
+		page,
+		pageConfigurationPage,
+		pageEditorPage,
+		pagesAdminPage,
+	}) => {
+		await test.step('Go to configure My Page and create a custom event', async () => {
+			await pagesAdminPage.goto(site.friendlyUrlPath);
+
+			await pageConfigurationPage.goToSection(pageTitle, 'Design');
+
+			await page.getByRole('tab', {name: 'JavaScript'}).click();
+
+			const customEventContent = `Analytics.track('pageTitleEvent', {
+birthdate: "2021-11-25T14:36:30.685Z",
+category: "wetsuit",
+duration: "3600000",
+like: "true",
+price: "259.95",
+temp: "11",
+});`;
+
+			await page.getByPlaceholder('JavaScript').fill(customEventContent);
+
+			await pageConfigurationPage.save();
+		});
+
+		await test.step('Publish My Page and generate a custom event with attributes of different types', async () => {
+			await pageEditorPage.goto(layout, site.friendlyUrlPath);
+
+			await pageEditorPage.publishPage();
+
+			await navigateToSitePage({
+				page,
+				pageName: pageTitle,
+				siteName,
+			});
+
+			await page.reload();
+
+			await page.waitForTimeout(5000);
+
+			await closeSessions(apiHelpers, page);
+		});
+
+		await test.step('Go to Analytics Cloud and Switch the property', async () => {
+			await navigateToACPageViaURL({
+				acPage: ACPage.eventAnalysisPage,
+				channelID: channel.id,
+				page,
+				projectID: project.groupId,
+			});
+		});
+
+		await test.step('Add the custom event, create an analysis and add a brekdown', async () => {
+			await page.getByRole('link', {name: 'Create Analysis'}).click();
+		});
+
+		await test.step('Add a name to the segment', async () => {
+			await setEventAnalysisName({
+				eventAnalysisName: `Event Analysis ${randomString}`,
+				page,
+			});
+		});
+
+		await test.step('Add the custom event to the analysis', async () => {
+			await addCustomEvent({
+				customEventName: 'pageTitleEvent',
+				page,
+			});
+		});
+
+		const attributeNameList = [
+			'category',
+			'duration',
+			'temp',
+		];
+
+		await test.step('Add a breakdown to the analysis', async () => {
+			for (const attributeName of attributeNameList) {
+				await addBreakdown({
+					breakdownName: attributeName,
+					page,
+					tab: 'Event',
+				});
+	
+			}
+		});
+
+		await test.step('Change the time filter to Last 24 hours', async () => {
+			await changeTimeFilter({
+				page,
+				timeFilterPeriod: 'Last 24 hours',
+			});
+		});
+
+		await test.step('Add a filter to the analysis', async () => {
+			await addFilter({
+				filterName: 'temp',
+				operator: 'is greater than',
+				page,
+				input: '10',
+			});
+
+			
+		});
+
+		await test.step('Check the analysis result appears and save the analysis', async () => {
+			await page.setViewportSize({height: 1080, width: 1920});
+
+			for (const attributeName of attributeNameList) {
+				await expect(page.getByRole('button', { name: `EVENT ${attributeName}`, exact: true })).toBeVisible();
+			}
+
+			const tableInfo = page.getByRole('cell');
+
+			await expect(tableInfo.getByText('wetsuit')).toBeVisible();
+			await expect(tableInfo.getByText('Between 01:00:00 - 01:00:59')).toBeVisible();
+			await expect(tableInfo.getByText('10 - 19')).toBeVisible();
+			await expect(tableInfo.getByText('100%')).toBeVisible();
+
+
+			await page.getByText('Save Analysis').click();
+		});
+
+		await test.step('Check the analysis result appears and save the', async () => {
+			await expect(page.getByText(`Event Analysis ${randomString}`)).toBeVisible();
+		});
+
+		await test.step('Navigation to attributes tab', async () => {
+			await navigateToACSettingsViaURL({
+				acPage: ACPage.eventAttributesPage,
+				page,
+				projectID: project.groupId,
+			});
+
+			await page.getByRole('link', { name: 'Attributes', exact: true }).click();
+		});
+
+		await test.step('Edit the new attribute', async () => {
+
+			await page.getByPlaceholder('Search').fill('temp');
+
+			await page.keyboard.press('Enter');
+
+			await expect(page.getByText('NUMBER')).toBeVisible();
+
+			await page.getByRole('link', { name: 'temp' }).click();
+
+			await page.getByRole('button', {name: 'Edit'}).click();
+
+			await page.getByLabel('Default Data Typecast').click();
+
+			await selectAndExpectToHaveValue({
+				optionValue: 'DATE',
+				select: page.getByLabel('Default Data Typecast'),
+			});
+			
+			await page.getByRole('button', { name: 'Save' }).click();
+		});
+
+		await test.step('Check that the type of the attribute has been changed', async () => {
+			await navigateToACSettingsViaURL({
+				acPage: ACPage.eventAttributesPage,
+				page,
+				projectID: project.groupId,
+			});
+
+			await page.getByRole('link', { name: 'Attributes', exact: true }).click();
+
+			await page.getByPlaceholder('Search').fill('temp');
+
+			await page.keyboard.press('Enter');
+
+			await expect(page.getByText('DATE')).toBeVisible();
+		});
+
+		await test.step('test', async () => {
+			await navigateToACPageViaURL({
+				acPage: ACPage.eventAnalysisPage,
+				channelID: channel.id,
+				page,
+				projectID: project.groupId,
+			});
+
+			await page.getByRole('link', { name: 'Event Analysis', exact: false}).click();
+			
+		});
+
+		await test.step('Check the analysis result appears', async () => {
+			for (const attributeName of attributeNameList) {
+				await expect(page.getByRole('button', { name: `EVENT ${attributeName}`, exact: true })).toBeVisible();
+			}
+
+			const tableInfo = page.getByRole('cell');
+
+			await expect(tableInfo.getByText('wetsuit')).toBeVisible();
+			await expect(tableInfo.getByText('Between 01:00:00 - 01:00:59')).toBeVisible();
+			await expect(tableInfo.getByText('10 - 19')).toBeVisible();
+			await expect(tableInfo.getByText('100%')).toBeVisible();
+
+		});
+
+		await test.step('Check that the attributes used have not had the attribute type changed', async () => {
+			await expect(page.getByRole('button', { name: 'Event | temp is greater than' })).toBeVisible();
+
+		});
+
+	});
 
 test(
 	'The analysis result should not appear if any of the attribute conditions are not contained within the result',
@@ -209,7 +427,7 @@ test(
 				filterName: 'pageTitle',
 				operator: 'does not contain',
 				page,
-				pageTitle: `${pageTitle} - ${siteName}`,
+				input: `${pageTitle} - ${siteName}`,
 			});
 		});
 
