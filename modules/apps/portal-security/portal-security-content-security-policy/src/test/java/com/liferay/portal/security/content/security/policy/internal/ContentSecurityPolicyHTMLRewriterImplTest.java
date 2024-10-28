283913df43f7b6203c17348890fde1328dd4d3b5
Brian Chan <brian.chan@liferay.com>
2024-10-28 12:06:35 -0500

    LPD-40070 auto SF

--- modules/apps/portal-security/portal-security-content-security-policy/src/test/java/com/liferay/portal/security/content/security/policy/internal/ContentSecurityPolicyHTMLRewriterImplTest.java
+++ modules/apps/portal-security/portal-security-content-security-policy/src/test/java/com/liferay/portal/security/content/security/policy/internal/ContentSecurityPolicyHTMLRewriterImplTest.java
@@ -47,14 +47,21 @@ public class ContentSecurityPolicyHTMLRewriterImplTest {
 	}
 
 	@Test
-	public void testRewriteInlineEventHandlersWithId() {
-		String html = _rewriteInlineEventHandlers(
-			"<div id=\"TEST_ID\" onclick=\"alert(1);\">hey</div>",
-			false);
+	public void testRewriteInlineEventHandlersRecursive() {
+		String html =
+			"<body onload=\"alert(1);\"><div onclick=\"alert(2);\">hey</div>" +
+				"</body>";
 
+		Assert.assertFalse(
+			_matches(
+				_rewriteInlineEventHandlers(html, false),
+				".*<script nonce=\"TEST_NONCE\">.*onclick.*alert\\(2\\);.*" +
+					"</script>.*"));
 		Assert.assertTrue(
-			_matches(html, ".*document\\.getElementById\\('TEST_ID'\\).*"));
-		Assert.assertTrue(_matches(html, "<div id=\"TEST_ID\">.*</div>.*"));
+			_matches(
+				_rewriteInlineEventHandlers(html, true),
+				".*<script nonce=\"TEST_NONCE\">.*onclick.*alert\\(2\\);.*" +
+					"</script>.*"));
 	}
 
 	@Test
@@ -93,6 +100,16 @@ public class ContentSecurityPolicyHTMLRewriterImplTest {
 	}
 
 	@Test
+	public void testRewriteInlineEventHandlersWithId() {
+		String html = _rewriteInlineEventHandlers(
+			"<div id=\"TEST_ID\" onclick=\"alert(1);\">hey</div>", false);
+
+		Assert.assertTrue(
+			_matches(html, ".*document\\.getElementById\\('TEST_ID'\\).*"));
+		Assert.assertTrue(_matches(html, "<div id=\"TEST_ID\">.*</div>.*"));
+	}
+
+	@Test
 	public void testRewriteInlineEventHandlersWithMultipleTopNodes() {
 		String html = _rewriteInlineEventHandlers(
 			"<div onclick=\"alert(1);\">hey</div><div onclick=\"alert(2);\">" +
@@ -106,24 +123,6 @@ public class ContentSecurityPolicyHTMLRewriterImplTest {
 					"onclick.*alert\\(2\\);.*</script>.*"));
 	}
 
-	@Test
-	public void testRewriteInlineEventHandlersRecursive() {
-		String html =
-			"<body onload=\"alert(1);\"><div onclick=\"alert(2);\">hey</div>" +
-				"</body>";
-
-		Assert.assertFalse(
-			_matches(
-				_rewriteInlineEventHandlers(html, false),
-				".*<script nonce=\"TEST_NONCE\">.*onclick.*alert\\(2\\);.*" +
-					"</script>.*"));
-		Assert.assertTrue(
-			_matches(
-				_rewriteInlineEventHandlers(html, true),
-				".*<script nonce=\"TEST_NONCE\">.*onclick.*alert\\(2\\);.*" +
-					"</script>.*"));
-	}
-
 	private boolean _matches(String html, String regexp) {
 		Pattern pattern = Pattern.compile(regexp, Pattern.DOTALL);
 
@@ -132,9 +131,7 @@ public class ContentSecurityPolicyHTMLRewriterImplTest {
 		return matcher.matches();
 	}
 
-	private String _rewriteInlineEventHandlers(
-		String html, boolean recursive) {
-
+	private String _rewriteInlineEventHandlers(String html, boolean recursive) {
 		return _contentSecurityPolicyHTMLRewriterImpl.
 			rewriteInlineEventHandlers(html, "TEST_NONCE", recursive);
 	}
