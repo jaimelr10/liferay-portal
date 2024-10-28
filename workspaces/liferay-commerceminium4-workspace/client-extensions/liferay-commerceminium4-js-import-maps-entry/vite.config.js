808850aed5d6c4b8293d69cb94630176606d3efe
Gianmarco Brunialti <gianmarco.brunialti@liferay.com>
2024-10-28 09:33:31 +0100

    LPD-40451 Broken vite build path for Minium 4 JS import map CX

--- workspaces/liferay-commerceminium4-workspace/client-extensions/liferay-commerceminium4-js-import-maps-entry/vite.config.js
+++ workspaces/liferay-commerceminium4-workspace/client-extensions/liferay-commerceminium4-js-import-maps-entry/vite.config.js
@@ -3,23 +3,23 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import react from '@vitejs/plugin-react';
+import {resolve} from 'path';
+
 /* eslint-disable no-undef */
 
 import {defineConfig} from 'vite';
-import {resolve} from 'path';
-import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 
 export default defineConfig({
 	base: '/o/vite-project',
 	build: {
-		assetsDir: 'static',
 		lib: {
 			entry: resolve(__dirname, 'src/index.js'),
 			formats: ['es'],
 		},
-		outDir: 'build',
+		outDir: 'build/static',
 
 		rollupOptions: {
 			external: [/@clayui\/*/, 'react', 'react-dom'],
