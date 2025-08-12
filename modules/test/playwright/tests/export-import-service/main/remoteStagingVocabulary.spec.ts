a5c3367deef32b6a237058f30c3c26d09bbfc43b
Jaime Leon <jaime.leon@liferay.com>
2025-08-12 15:56:11 +0200

    Test if other staging is making this fail

--- modules/test/playwright/tests/export-import-service/main/remoteStagingVocabulary.spec.ts
+++ modules/test/playwright/tests/export-import-service/main/remoteStagingVocabulary.spec.ts
@@ -19,8 +19,6 @@ import {webContentDisplayPageTest} from '../../../fixtures/webContentDisplayPage
 import getGlobalSiteId from '../../../utils/getGlobalSiteId';
 import getRandomString from '../../../utils/getRandomString';
 import {PORTLET_URLS} from '../../../utils/portletUrls';
-import {reloadUntilVisible} from '../../../utils/reloadUntilVisible';
-import getBasicWebContentStructureId from '../../../utils/structured-content/getBasicWebContentStructureId';
 import {pagesPagesTest} from '../../layout-admin-web/main/fixtures/pagesPagesTest';
 import {remoteStagingPagesTest} from './fixtures/remoteStagingPagesTest';
 import {safeTeardown} from './utils/safeTeardown';
@@ -43,101 +41,6 @@ export const test = mergeTests(
 	remoteStagingPagesTest,
 	uiElementsPageTest,
 	webContentDisplayPageTest
-);
-
-test(
-	'check web contents can be published via their portlet using remote staging',
-	{tag: '@LPS-81950'},
-	async ({
-		apiHelpers,
-		pageEditorPage,
-		remoteApiHelpers,
-		remotePage,
-		remoteStagingPage,
-		uiElementsPage,
-		webContentDisplayPage,
-		widgetPagePage,
-	}) => {
-		test.slow();
-
-		const site = await apiHelpers.headlessSite.createSite({
-			name: 'Site Name',
-		});
-
-		apiHelpers.data.push({id: site.id, type: 'site'});
-
-		const layout = await apiHelpers.jsonWebServicesLayout.addLayout({
-			groupId: site.id,
-			options: {
-				type: 'portlet',
-			},
-			title: 'Staging Test Page',
-		});
-
-		const remoteSite = await remoteApiHelpers.headlessSite.createSite({
-			name: 'Remote Site Name',
-		});
-
-		remoteApiHelpers.data.push({id: remoteSite.id, type: 'site'});
-
-		await apiHelpers.jsonWebServicesStaging.enableRemoteStaging({
-			groupId: site.id,
-			remoteGroupId: remoteSite.id,
-			remotePort,
-		});
-
-		await remoteStagingPage.publishToLive({
-			layoutFriendlyURL: layout.friendlyURL,
-			siteFriendlyUrl: site.friendlyUrlPath,
-		});
-
-		const basicWebContentStructureId =
-			await getBasicWebContentStructureId(apiHelpers);
-
-		await apiHelpers.jsonWebServicesJournal.addWebContent({
-			content: 'WC WebContent Content',
-			ddmStructureId: basicWebContentStructureId,
-			groupId: site.id,
-			titleMap: {en_US: 'WC WebContent Title'},
-		});
-
-		await pageEditorPage.goto(layout, site.friendlyUrlPath);
-		await uiElementsPage.addButton.click();
-		await widgetPagePage.addPortlet(
-			'Web Content Display',
-			'Content Management'
-		);
-		await webContentDisplayPage.addWebContentWithDisplay({
-			pageType: 'widget',
-			webContentName: 'WC WebContent Title',
-		});
-
-		await remoteStagingPage.publishToLive({
-			layoutFriendlyURL: layout.friendlyURL,
-			siteFriendlyUrl: site.friendlyUrlPath,
-		});
-
-		const remoteUrl = remoteApiHelpers.baseUrl.substring(
-			0,
-			remoteApiHelpers.baseUrl.length - 3
-		);
-
-		await remotePage.goto(`${remoteUrl}/web${remoteSite.friendlyUrlPath}`);
-
-		await reloadUntilVisible({
-			myLocator: remotePage.getByRole('heading', {
-				name: 'WC WebContent Title',
-			}),
-			page: remotePage,
-		});
-
-		await expect(
-			remotePage.getByRole('heading', {name: 'WC WebContent Title'})
-		).toBeVisible();
-		await expect(
-			remotePage.getByText('WC WebContent Content')
-		).toBeVisible();
-	}
 );
 
 test(
