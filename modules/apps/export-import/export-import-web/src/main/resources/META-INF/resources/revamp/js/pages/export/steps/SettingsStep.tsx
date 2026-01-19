71694be4adf23bc73a45e2c3cfbcaf904702bb03
Jaime Leon <jaime.leon@liferay.com>
2026-01-19 13:21:13 +0100

    wip

---
+++ modules/apps/export-import/export-import-web/src/main/resources/META-INF/resources/revamp/js/pages/export/steps/SettingsStep.tsx
@@ -0,0 +1,39 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import ClayLayout from '@clayui/layout';
+import React from 'react';
+
+import BasePage from '../../../components/BasePage';
+import Footer from '../../../components/Footer';
+import {ESteps, IGenericStepProps} from '../../../components/Wizard';
+
+const Step: React.FC<
+	{children?: React.ReactNode | undefined} & IGenericStepProps
+> = ({backURL, onChangeStep}) => (
+	<>
+		<BasePage
+			description={Liferay.Language.get(
+				'Configure-your-Export-Settings.'
+			)}
+			title={Liferay.Language.get('settings')}
+		>
+			<ClayLayout.Sheet>
+				<ClayLayout.SheetHeader>
+					{Liferay.Language.get('export-details')}
+				</ClayLayout.SheetHeader>
+			</ClayLayout.Sheet>
+		</BasePage>
+
+		<Footer
+			backURL={backURL}
+			exportURL="URLPlaceholder"
+			onChangeStep={onChangeStep}
+			previousStep={ESteps.DataSelection}
+		/>
+	</>
+);
+
+export default Step;
