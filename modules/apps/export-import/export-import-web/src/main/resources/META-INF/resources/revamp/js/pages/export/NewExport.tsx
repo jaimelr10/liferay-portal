635f4be67fecdb2ed81d86f4cc7be7be7224824e
Jaime Leon <jaime.leon@liferay.com>
2026-01-20 10:13:44 +0100

    LPD-76553 Add wizard to the new export component

--- modules/apps/export-import/export-import-web/src/main/resources/META-INF/resources/revamp/js/pages/export/NewExport.tsx
+++ modules/apps/export-import/export-import-web/src/main/resources/META-INF/resources/revamp/js/pages/export/NewExport.tsx
@@ -3,13 +3,39 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import ClayLayout from '@clayui/layout';
 import React from 'react';
 
-export function NewExport() {
-	return (
-		<ClayLayout.Sheet>
-			<h1>New Export UI</h1>
-		</ClayLayout.Sheet>
-	);
+import {
+	ESteps,
+	IGenericStepProps,
+	IStepProps,
+	Wizard,
+} from '../../components/Wizard';
+import DataSelectionStep from './steps/DataSelectionStep';
+import SettingsStep from './steps/SettingsStep';
+import SetupStep from './steps/SetupStep';
+
+const STEPS: IStepProps<IGenericStepProps, ESteps>[] = [
+	{
+		Component: SetupStep,
+		available: true,
+		key: ESteps.Setup,
+		title: Liferay.Language.get('setup'),
+	},
+	{
+		Component: DataSelectionStep,
+		available: false,
+		key: ESteps.DataSelection,
+		title: Liferay.Language.get('data-selection'),
+	},
+	{
+		Component: SettingsStep,
+		available: false,
+		key: ESteps.Settings,
+		title: Liferay.Language.get('settings'),
+	},
+];
+
+export function NewExport({backURL}: {backURL: string}) {
+	return <Wizard backURL={backURL} externalSteps={STEPS} />;
 }
