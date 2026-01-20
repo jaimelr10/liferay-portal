9bb75ce95da025ed7dc6b3a3775f92502a04aef6
Jaime Leon <jaime.leon@liferay.com>
2026-01-20 09:50:21 +0100

    LPD-76553 Create multistep footer

---
+++ modules/apps/export-import/export-import-web/src/main/resources/META-INF/resources/revamp/js/components/Footer.tsx
@@ -0,0 +1,60 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import ClayButton from '@clayui/button';
+import ClayIcon from '@clayui/icon';
+import ClayLink from '@clayui/link';
+import React from 'react';
+
+import BasePage from '../components/BasePage';
+import {ESteps, IGenericStepProps} from '../components/Wizard';
+
+const Footer: React.FC<
+	{children?: React.ReactNode | undefined} & IGenericStepProps
+> = ({backURL, exportURL, nextStep, onChangeStep, previousStep}) => (
+	<BasePage.Footer>
+		{previousStep !== undefined && (
+			<ClayButton
+				displayType="unstyled"
+				onClick={() => onChangeStep(previousStep)}
+			>
+				<span className="inline-item inline-item-before">
+					<ClayIcon symbol="order-arrow-left" />
+
+					{Liferay.Language.get('previous')}
+				</span>
+			</ClayButton>
+		)}
+
+		<div className="d-flex justify-content-end w-100">
+			<ClayLink
+				button
+				className="mr-2"
+				displayType="secondary"
+				href={backURL}
+			>
+				{Liferay.Language.get('cancel')}
+			</ClayLink>
+
+			{nextStep && (
+				<ClayButton onClick={() => onChangeStep(nextStep)}>
+					{Liferay.Language.get('continue')}
+				</ClayButton>
+			)}
+
+			{exportURL && (
+				<ClayButton onClick={() => onChangeStep(ESteps.Settings)}>
+					<span className="inline-item inline-item-before">
+						<ClayIcon className="mr-1" symbol="export" />
+
+						{Liferay.Language.get('export')}
+					</span>
+				</ClayButton>
+			)}
+		</div>
+	</BasePage.Footer>
+);
+
+export default Footer;
