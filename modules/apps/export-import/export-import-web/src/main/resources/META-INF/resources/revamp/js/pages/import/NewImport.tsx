f408f78d049e27ec8e6432e54dd4b80443317eca
Jaime Leon <jaime.leon@liferay.com>
2026-01-26 12:03:25 +0100

    LPD-77231 Add import wizard

--- modules/apps/export-import/export-import-web/src/main/resources/META-INF/resources/revamp/js/pages/import/NewImport.tsx
+++ modules/apps/export-import/export-import-web/src/main/resources/META-INF/resources/revamp/js/pages/import/NewImport.tsx
@@ -1,15 +1,48 @@
 /**
- * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import ClayLayout from '@clayui/layout';
 import React from 'react';
 
-export function NewImport() {
+import {Wizard, WizardStep} from '../../components/Wizard';
+import DataSelectionStep from './steps/DataSelectionStep';
+import FileSelectionStep from './steps/FileSelectionStep';
+import SettingsStep from './steps/SettingsStep';
+
+export function NewImport({backURL}: {backURL: string}) {
 	return (
-		<ClayLayout.Sheet>
-			<h1>New Import UI</h1>
-		</ClayLayout.Sheet>
+		<Wizard
+			backURL={backURL}
+			onSubmit={() => {}}
+			onSubmitLabel={Liferay.Language.get('import')}
+		>
+			<WizardStep
+				description={Liferay.Language.get(
+					'name-your-import-process-and-upload-your-file'
+				)}
+				title={Liferay.Language.get('setup')}
+			>
+				<FileSelectionStep />
+			</WizardStep>
+
+			<WizardStep
+				description={Liferay.Language.get(
+					'select-the-data-from-your-file-that-you-would-like-to-import'
+				)}
+				title={Liferay.Language.get('data-selection')}
+			>
+				<DataSelectionStep />
+			</WizardStep>
+
+			<WizardStep
+				description={Liferay.Language.get(
+					'set-up-your-import-configuration'
+				)}
+				title={Liferay.Language.get('settings')}
+			>
+				<SettingsStep />
+			</WizardStep>
+		</Wizard>
 	);
 }
